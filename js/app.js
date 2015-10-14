25337cb657035489abaef842709311a1b7da7db3
Rich Donnellan <rich.donnellan@gmail.com>
2015-10-14 11:31:02 -0700

    Update jQuery animations and add Die constructor

--- js/app.js
+++ js/app.js
@@ -1,13 +1,36 @@
 console.log('Script initialized.'); // Initial log to confirm script is working
 
+
+
 //======= Game object begin =========//
 var game = {
+  maxPlayers: 4, // number of players option
   players: [],
   dice: [],
   //playerTurn: '',
-  //rollsLeft: 3,
-  //round: 0,
+  rollsLeft: 3, // max amount of rolls
+  //round: 13, // max rounds
   //winner: null,
+
+  // Initialize the game
+  init: function () {
+    $('#add-player').hide().fadeIn(1500); // Hide and fadeIn add player button
+    $('#dice-board').hide(); // Hide the dice board until game started
+    $('#score-card, #roll-dice').hide(); // Hide the score card until game started
+    $('#start-game').hide(); // Hide start button until players are added
+    $('#players-display').hide(); // Hide players until start game is called
+    $('#new-game').hide(); // Hide new game button until game is started
+    $('#name-input').on('keypress', function (e) { // allow keyboard entry to add player
+      if (e.which === 13) {
+        game.addPlayer();
+      }
+    }); // call add player method if hitting enter after input
+
+    $('#add-btn').on('click', game.addPlayer); // call add player method if button clicked
+    $('#start-btn').on('click', game.startGame); // call add player method
+    $('#roll-btn').on('click', game.rollDice); // call roll dice method if button clicked
+    $('#new-btn').on('click', game.newGame); // call new game method if button clicked
+  },
 
   // Create a new player object and add player to the players array
   addPlayer: function () {
@@ -18,35 +41,63 @@ var game = {
     console.log($name.val()); // check input value
     if ($name.val() === '') {
       alert('Please type your name for a more personalized experience.');
-    } else {
+    } else if (game.players.length < game.maxPlayers) {
         newPlayer.name = $name.val(); // set object name property to input value
         $name.val(''); // clear input value
         game.players.push(newPlayer); // push entry to players array => Object
         console.log('A new player has been added.'); // confirmation message
+        $('#start-game').fadeIn(1000);
+      } else {
+        alert('Sorry. A max of ' + game.maxPlayers + ' players is supported at this time.');
       }
-    //if (game.players.length > 2) {
-      //alert('Sorry. A max of two players is supported at this time.');
-    //}
   },
 
   // Start game functionality
   startGame: function () {
+    $('#rolls-left').append(game.rollsLeft);
+    $('#add-btn').off('click', game.addPlayer); // remove add player click event
+    $('#start-btn').off('click', game.startGame); // remove start game click event
+    $('#add-player, #start-game').fadeOut();
+    $('#dice-board').delay(250).fadeIn(1000);
     $.each(game.players, function(index) {
-      $('#players-display').append('<span>' + game.players[index].name + ': ' + game.players[index].score + '</span>');
+      $('#players-display').append('<span class="player-name">' + game.players[index].name + ':</span> ' + '<span class="player-score">' + game.players[index].score + '</span>').fadeIn();
     });
-    console.log(firstPlayer);
+    $('#score-card').delay(1200).fadeIn(1000);
+    $('#new-game').delay(2000).fadeIn();
+  },
+
+  newGame: function () {
+    game.players = [];
+    $('#players-display').fadeOut();
+  },
+
+  createDice: function () {
+
   },
 
   // Dice roll functionality
-  rollDice: function () {},
+  rollDice: function () {
+    if (game.rollsLeft === 0) {
+      console.log('Get next player');
+      alert('Get next player ...');
+    } else {
+        console.log('Clicked');
+        game.rollsLeft--;
+        $('#rolls-left').text(game.rollsLeft);
+      }
+  },
 
   // Get winner functionality
-  getWinner: function () {},
+  getWinner: function () {
+    if (game.players.length === 1) {
+      alert('Your score is: ' + player.score);
+    } else {
+      console.log(Math.max.apply(null, game.players));
+    }
+  },
 
   // Get next player functionality
   nextPlayer: function () {
-    var firstPlayer = game.players[0];
-    console.log(firstPlayer);
   }
 
 };
@@ -60,21 +111,16 @@ function Player() {
   this.pieces = {};
 }
 
-//======= player object begin =========//
+//======= player object end =========//
 
-$('#name-input').on('keypress', function (e) {
-  if (e.which === 13) {
-    game.addPlayer();
-  }
-}); // call add player method if hitting enter after input
+//======= die object begin =========//
+function Die () {
+  this.value = null;
+  this.roll = function () {
+    this.value = Math.floor((Math.random() * 6)) + 1;
+    return this.value;
+  };
+}
+//======= die object end =========//
 
-$('#add-player').on('click', game.addPlayer); // call add player method if button clicked
-$('#start-game').on('click', game.startGame); // call add player method
-
-
-//   // Create dice object
-//   function Dice() {
-//       number = function() {
-//         return console.log(Math.floor((Math.random() * 6)) + 1);
-//       }
-//   }
+game.init();
